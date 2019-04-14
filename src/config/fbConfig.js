18753aec2d94217617460ec9a826ed244d690e7b
Kevin <yardman10550@gmail.com>
2019-04-14 17:34:52 -0400

    Connect app to firestore, add test data to firetsore

---
+++ src/config/fbConfig.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCns6BzmPdTEQ9ddql_3TmVZa1YJKKDfD0",
+    authDomain: "kevin-projectmanager.firebaseapp.com",
+    databaseURL: "https://kevin-projectmanager.firebaseio.com",
+    projectId: "kevin-projectmanager",
+    storageBucket: "kevin-projectmanager.appspot.com",
+    messagingSenderId: "1074247434093"
+};
+firebase.initializeApp(config);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
+
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,17 @@
 export const createProject = (project) => {
-    return (dispatch, getState) => {
+    return (dispatch, getState, { getFirebase, getFirestore }) => {
         // make async call to database 
-        dispatch({ type: 'CREATE_PROJECT', project });
+        const firstore = getFirestore();
+        firstore.collection('projects').add({
+            ...project,
+            authorFirstName: 'Jann',
+            authorLastName: 'Meikle',
+            authorId: 123456,
+            timestamp: new Date()
+        }).then(() => {
+            dispatch({ type: 'CREATE_PROJECT', project });
+        }).catch((err) => {
+            dispatch({ type: 'CREATE_PROJECT_ERROR', err })
+        })
     }
 }
